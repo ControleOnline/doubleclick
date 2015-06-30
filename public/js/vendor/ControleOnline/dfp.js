183d4e56d24f5b393b38598f9b0046a625a0d2e2
luiz.kim <luiz.kim@esferabr133.fttx.grupo>
2015-06-30 18:17:54 -0300

    Add Media Query on Widgets

--- public/js/vendor/ControleOnline/dfp.js
+++ public/js/vendor/ControleOnline/dfp.js
@@ -16,33 +16,41 @@ var DFP = {
         });
         this.replaceBanners();
     },
-    show: function (slot, size, dfp_id) {
+    show: function (slot, size, dfp_id, min_width, max_width) {
         var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        if (size[0] <= w && size[1] <= h) {
-            var d = document.createElement('div');
-            d.setAttribute('id', dfp_id);
-            d.setAttribute('style', 'width:' + size[0] + 'px; height:' + size[1] + 'px;');
-            var s = document.createElement('script');
-            s.setAttribute('type', 'text/javascript');
-            s.innerHTML = 'googletag.cmd.push(function() { ' +
-                    'googletag.defineSlot(\'' + slot + '\', [' + size[0] + ',' + size[1] + '], \'' + dfp_id + '\').addService(googletag.pubads());' +
-                    'googletag.display(\'' + dfp_id + '\');' +
-                    '});';
-            d.appendChild(s);
-            return d;
+        if (w >= min_width && (max_width === '0' || w <= max_width)) {
+            if (size[0] <= w && size[1] <= h) {
+                var d = document.createElement('div');
+                d.setAttribute('id', dfp_id);
+                if (size[0] > 1 && size[1] > 1) {
+                    d.setAttribute('style', 'width:' + size[0] + 'px; height:' + size[1] + 'px;');
+                }
+                var s = document.createElement('script');
+                s.setAttribute('type', 'text/javascript');
+                s.innerHTML = 'googletag.cmd.push(function() { ' +
+                        'googletag.defineSlot(\'' + slot + '\', [' + size[0] + ',' + size[1] + '], \'' + dfp_id + '\').addService(googletag.pubads());' +
+                        'googletag.display(\'' + dfp_id + '\');' +
+                        '});';
+                d.appendChild(s);
+                return d;
+            }
         }
     },
     replaceBanners: function () {
         var banners = document.getElementsByClassName('dfp'), d, content;
         for (var i = 0; i < banners.length; ++i) {
             d = banners[i];
-            content = this.show(d.getAttribute('data-slot'), JSON.parse(d.getAttribute('data-size')), d.getAttribute('data-id'));
-            if (content && !d.innerHTML) {
-                d.innerHTML = '<!-- ' + d.getAttribute('data-slot') + ' -->';
-                d.appendChild(content);
-                //d.classList.add('dfp-loaded');
-                //d.classList.remove('dfp');
+            content = this.show(d.getAttribute('data-slot'), JSON.parse(d.getAttribute('data-size')), d.getAttribute('data-id'), d.getAttribute('min-width'), d.getAttribute('max-width'));
+            if (content) {
+                if (!d.innerHTML) {
+                    d.innerHTML = '<!-- ' + d.getAttribute('data-slot') + ' -->';
+                    d.appendChild(content);
+                    //d.classList.add('dfp-loaded');
+                    //d.classList.remove('dfp');
+                }
+            } else {
+                d.parentNode.removeChild(d);
             }
         }
     }
