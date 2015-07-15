eeb8dff2370aff3558716cd1c6447f595a75ae4f
luiz.kim <luiz.kim@esferabr133.fttx.grupo>
2015-07-15 18:51:11 -0300

    Change Gads Event Track

--- public/js/vendor/ControleOnline/dfp.js
+++ public/js/vendor/ControleOnline/dfp.js
@@ -169,8 +169,8 @@ var DFP = {
                 '&r="' + '+encodeURIComponent(document.location.pathname)+"' +
                 '&_="' + '+(new Date().getTime() / 1000), true);' +
                 'xmlhttp.send();' +
-                'if (typeof _gaq !== \'undefined\') {' +                
-                '_gaq.push([\'_trackEvent\', \'Adblock\', \'Blocked\', \'Ads\',  ' + this.countAds + ', true]);' +
+                'if (typeof _gaq !== \'undefined\') {' +
+                '_gaq.push([\'_trackEvent\',\'Adblock\',\'Blocked\',\'Ads\',' + this.countAds + ', true]);' +
                 '}' +
                 '}' +
                 '}, 2000);';
